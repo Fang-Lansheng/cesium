3a3263abdcef41762fa951edd155b7fffad3f863
Thistledown <120768091@qq.com>
2018-11-03 10:31:33 +0800

    新增按钮

--- Apps/Study/js/Study.js
+++ Apps/Study/js/Study.js
@@ -249,13 +249,64 @@ function createModel(url, id,  height) {
   <h1>教室模型</h1>\
   <p>这是一个教室模型！</p>'
 }
+// rain
+var rainParticleSize = scene.drawingBufferWidth / 80.0;
+var rainRadius = 100000.0;
+var rainImageSize = new Cesium.Cartesian2(rainParticleSize, rainParticleSize * 2.0);
+var rainSystem;
 
-Sandcastle.addToolbarButton('加载教室模型', function() {
-  createModel('../SampleData/models/classroom_dae.gltf', 'classroom', 0);
+var rainGravityScratch = new Cesium.Cartesian3();
+var rainUpdate = function(particle, dt) {
+    rainGravityScratch = Cesium.Cartesian3.normalize(particle.position, rainGravityScratch);
+    rainGravityScratch = Cesium.Cartesian3.multiplyByScalar(rainGravityScratch, -1050.0, rainGravityScratch);
+
+    particle.position = Cesium.Cartesian3.add(particle.position, rainGravityScratch, particle.position);
+
+    var distance = Cesium.Cartesian3.distance(scene.camera.position, particle.position);
+    if (distance > rainRadius) {
+        particle.endColor.alpha = 0.0;
+    } else {
+        particle.endColor.alpha = rainSystem.endColor.alpha / (distance / rainRadius + 0.1);
+    }
+};
+
+rainSystem = new Cesium.ParticleSystem({
+    modelMatrix : new Cesium.Matrix4.fromTranslation(scene.camera.position),
+    speed : -1.0,
+    lifetime : 15.0,
+    emitter : new Cesium.SphereEmitter(rainRadius),
+    startScale : 1.0,
+    endScale : 0.0,
+    image : '../SampleData/circular_particle.png',
+    emissionRate : 9000.0,
+    startColor :new Cesium.Color(0.27, 0.5, 0.70, 0.0),
+    endColor : new Cesium.Color(0.27, 0.5, 0.70, 0.98),
+    imageSize : rainImageSize,
+    updateCallback : rainUpdate
 });
-Sandcastle.addToolbarButton('清除模型', function() {
-  viewer.entities.removeById('classroom');
-})
+scene.primitives.add(rainSystem); 
+
+function ClearWeather() {
+  rainSystem.show = false;
+  alert('已关闭天气');
+}
+function Raining() {
+  scene.skyAtmosphere.hueShift = -0.97;
+  scene.skyAtmosphere.saturationShift = 0.25;
+  scene.skyAtmosphere.brightnessShift = -0.4;
+
+  scene.fog.density = 0.00025;
+  scene.fog.minimumBrightness = 0.01;
+
+  rainSystem.show = true;
+}
+
+// Sandcastle.addToolbarButton('加载教室模型', function() {
+//   createModel('../SampleData/models/classroom_dae.gltf', 'classroom', 0);
+// });
+// Sandcastle.addToolbarButton('清除模型', function() {
+//   viewer.entities.removeById('classroom');
+// })
 Sandcastle.finishedLoading();
 
 
