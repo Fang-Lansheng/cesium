7d19fb6dd6694b8e17a98aa6d75bc0e09864c294
Thistledown <120768091@qq.com>
2018-11-20 20:49:31 +0800

    copy完成

--- Apps/CesiumMeshVisualizer/js/index.js
+++ Apps/CesiumMeshVisualizer/js/index.js
@@ -19,7 +19,7 @@ var MeshPhongMaterial = Cesium.MeshPhongMaterial;
 var GeometryUtils = Cesium.GeometryUtils;
 var LOD = Cesium.LOD;
 var CSG = Cesium.CSG;
-homePosition[2] = 1000;
+homePosition[2] = 100;
 
 init();
 
@@ -28,10 +28,10 @@ var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center);
 
 var meshVisualizer = new MeshVisualizer({
 	modelMatrix: modelMatrix,
-	up: { z: 1 },
+	up: { y: 1 },
 	referenceAxisParameter: {
-		length: 1000, 
-		width: 5, 
+		length: 500, 
+		width: 0.1, 
 		headLength:2,
 		headWidth: 1.0
 	}
@@ -39,28 +39,34 @@ var meshVisualizer = new MeshVisualizer({
 viewer.scene.primitives.add(meshVisualizer);
 meshVisualizer.showReference = true;//显示坐标轴
 
-// 创建立方体
-function createBoxMesh(dmX, dmY, dmZ, scale) {
-	var dimensions = new Cesium.Cartesian3(dmX, dmY, dmZ);
-	var boxGeometry = Cesium.BoxGeometry.fromDimensions({
-		dimensions: dimensions,
-		vertexFormat: Cesium.VertexFormat.POSITION_AND_NORMAL
+// // 创建 groundMesh
+// var groundGeometry = Cesium.BoxGeometry.createGeometry(Cesium.BoxGeometry.fromDimensions({
+// 	dimensions: new Cesium.Cartesian3(2000, 0.1, 2000),
+// 	vertexFormat: Cesium.VertexFormat.POSITION_AND_NORMAL
+// }));
+// var groundMaterial = new MeshMaterial({
+// 	defaultColor: 'rgb(125, 125, 125)',
+// 	wireframe: false,
+// 	side: MeshMaterial.Sides.DOUBLE
+// });
+// var groundMesh = new Mesh(groundGeometry, groundMaterial);
+// groundMesh.position.z = -10;
+// meshVisualizer.add(groundMesh);
+
+function createRandomColor() {
+	return Cesium.Color.fromRandom({
+		alpha: 1
 	});
-	var material = new MeshPhongMaterial({
-		defaultColor: "rgb(125, 125, 125)"
-	});
-	var mesh = new Mesh(boxGeometry, material);
-	if (scale) {
-		mesh.scale.x = scale;
-		mesh.scale.y = scale;
-		mesh.scale.z = scale;
-	}
-	return mesh;
+	// fromRgba(Math.floor(Math.random() * (1 << 24)));
 }
 
-var groundMesh = createBoxMesh(1000*2, 1000*2 , 10*2, 1);
-groundMesh.position.z = -10;
-meshVisualizer.add(groundMesh);
+function createMaterial() {
+	return new MeshPhongMaterial({
+		defaultColor: createRandomColor(),
+		side: MeshMaterial.Sides.DOUBLE,
+		translucent: false
+	});
+}
 
 Cesium.Cartesian3.prototype.set = function (x, y, z) {
 	this.x = x; this.y = y; this.z = z;
@@ -69,6 +75,7 @@ Cesium.Quaternion.prototype.set = function (x, y, z, w) {
 	this.x = x; this.y = y; this.z = z; this.w = w;
 }
 
+// 基于 ammo.js 引擎（Bullet Physics Engine on JavaScript）
 Ammo().then(function() {
 	// 全局变量
 	var DISABLE_DEACTIVATION = 4;
@@ -94,10 +101,14 @@ Ammo().then(function() {
 	// 键盘事件
 	var actions = {};
 	var keyActions = {
-		'keyW': 'acceleration',
-		'keyS': 'braking',
-		'keyA': 'left',
-		'keyD': 'right'
+		'KeyW': 'acceleration',
+		'KeyS': 'braking',
+		'KeyA': 'left',
+		'KeyD': 'right',
+		'ArrowUp': 'acceleration',
+		'ArrowDown': 'braking',
+		'ArrowLeft': 'left',
+		'ArrowRight': 'right'
 	};
 
 	// 函数
@@ -117,16 +128,347 @@ Ammo().then(function() {
 		broadphase = new Ammo.btDbvtBroadphase();
 		solver = new Ammo.btSequentialImpulseConstraintSolver();
 		physicWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-		physicWorld.setGravity(new Ammo.btVector(0, -9.82, 0));
+		physicWorld.setGravity(new Ammo.btVector3(0, -9.82, 0));
 	}
 
 	function updatePhysics(deltaTime) {
 		for (let i = 0; i < syncList.length; i++) {
 			syncList[i](deltaTime);
 		}
-		physicWorld.stepSimultaion
+		physicWorld.stepSimulation(deltaTime, 10);
+		time += deltaTime;
 	}
-})
+
+	function keyup(e) {
+		if (keyActions[e.code]) {
+			actions[keyActions[e.code]] = false;
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		}
+	}
+
+	function keydown(e) {
+		if (keyActions[e.code]) {
+			actions[keyActions[e.code]] = true;
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		}
+	}
+
+	// 创建立方体盒子
+	function createBox(pos, quat, w, l, h, mass, friction) {
+	var material = createMaterial();	//= mass > 0 ? materialDynamic : materialStatic;
+	var shape = Cesium.BoxGeometry.fromDimensions({
+		dimensions: new Cesium.Cartesian3(w, l, h),
+		vertexFormat: new Cesium.VertexFormat({
+				position: true,
+				normal: true
+		})
+	});
+
+	var geometry = new Ammo.btBoxShape(new Ammo.btVector3(w * 0.5, l * 0.5, h * 0.5));
+
+	if (!mass) mass = 0;
+	if (!friction) friction = 1;
+
+	var mesh = new Mesh(shape, material);
+	Cesium.Cartesian3.clone(pos, mesh.position);
+	mesh.quaternion = new Cesium.Quaternion(quat.x, quat.y, quat.z, quat.w);
+	meshVisualizer.add(mesh);
+
+	var transform = new Ammo.btTransform();
+	transform.setIdentity();
+	transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+	transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+	var motionState = new Ammo.btDefaultMotionState(transform);
+
+	var localInertia = new Ammo.btVector3(0, 0, 0);
+	geometry.calculateLocalInertia(mass, localInertia);
+
+	var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, geometry, localInertia);
+	var body = new Ammo.btRigidBody(rbInfo);
+
+	body.setFriction(friction);
+	//body.setRestitution(.9);
+	//body.setDamping(0.2, 0.2);
+
+	physicWorld.addRigidBody(body);
+
+	if (mass > 0) {
+		body.setActivationState(DISABLE_DEACTIVATION);
+		// Sync physics and graphics
+		function sync(dt) {
+			var ms = body.getMotionState();
+			if (ms) {
+					ms.getWorldTransform(TRANSFORM_AUX);
+					var p = TRANSFORM_AUX.getOrigin();
+					var q = TRANSFORM_AUX.getRotation();
+					mesh.position.set(p.x(), p.y(), p.z());
+					mesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+					mesh.modelMatrixNeedsUpdate = true;
+			}
+		}
+
+		syncList.push(sync);
+	}
+}
+
+	// 创建小车车轮
+	function createWheelMesh(radius, width) {
+		var t = new Cesium.CylinderGeometry({		// 创建圆柱体
+			length: width,
+			topRadius: radius,
+			bottomRadius: radius,
+			slices: 24
+		});
+		// var t = new THREE.CylinderGeometry(radius, radius, width, 24, 1);
+
+		var mesh = new Mesh(t, materialInteractive);
+		GeometryUtils.rotateY(mesh.geometry, Math.PI / 2);
+		mesh.quaternion = new Cesium.Quaternion();	
+		// Cesium.Quaternion.fromAxisAngle(new Cesium.Cartesian3(0, 0, 1), Math.PI / 2);
+
+		var shape = Cesium.BoxGeometry.fromDimensions({
+			dimensions: new Cesium.Cartesian3(width * 1.5, radius * 1.75, radius * 0.25),
+			vertexFormat: new Cesium.VertexFormat({
+				position: true,
+				normal: true
+			})
+		});
+		var meshShape = new Mesh(shape, materialInteractive);
+
+		meshShape.quaternion = new Cesium.Quaternion();
+		// Cesium.Quaternion.fromAxisAngle(new Cesium.Cartesian3(0, 0, 1), 0);
+		mesh.add(meshShape);
+		meshVisualizer.add(mesh);
+		return mesh;
+	}
+
+	// 创建小车底座
+	function createChassisMesh(width, length, height) {
+		var shape = Cesium.BoxGeometry.fromDimensions({	// 创建立方体
+			dimensions: new Cesium.Cartesian3(width, length, height),
+			vertexFormat: new Cesium.VertexFormat({
+				position: true, 
+				normal: true
+			})
+		});
+
+		var mesh = new Mesh(shape, materialInteractive);
+		mesh.quaternion = new Cesium.Quaternion(0, 0, 0, 1);
+		meshVisualizer.add(mesh);
+		return mesh;
+	}
+
+	// 创建小车
+	function createVehicle(pos, quat) {
+		// 小车常量
+		var chassisWidth = 1.8;						// 车架宽度
+		var chassisHeight = 0.6;					// 车架高度
+		var chassisLength = 4;						// 车架长度
+		var massVehicle = 800;						// 汽车质量
+
+		var wheelAxisPositionBack = -1;		// 
+		var wheelRadiusBack = 0.4;
+		var wheelWidthBack = 0.3;
+		var wheelHalfTrackBack = 1;
+		var wheelAxisHeightBack = 0.3;
+
+		var wheelAxisFrontPosition = 1.7;
+		var wheelHalfTrackFront = 1;
+		var wheelAxisHeightFront = 0.3;
+		var wheelRadiusFront = 0.35;
+		var wheelWidthFront = 0.2;
+
+		var friction = 1000;							// 摩擦力
+		var suspensionStiffness = 20.0;		// 悬架刚度
+		var suspensionDamping = 2.3;			// 悬架阻尼
+		var suspensionCompression = 4.4;	// 
+		var suspensionRestLength = 0.6;		// 悬架长度（静止长度）
+		var rollInfluence = 0.2;
+
+		var steeringIncrement = 0.04;			// 转向增量
+		var steeringClamp = 0.5;					// 转向角度
+		var maxEngineForce = 2000;				// 最大牵引力
+		var maxBreakingForce = 100;				// 最大制动力
+
+		// 车辆底盘
+		var geometry = new Ammo.btBoxShape(new Ammo.btVector3(chassisWidth * 0.5, chassisHeight * 0.5, chassisLength * 0.5));
+		var transform = new Ammo.btTransform();
+		transform.setIdentity();
+		transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+		transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+		var motionState = new Ammo.btDefaultMotionState(transform);
+		var localInertia = new Ammo.btVector3(0, 0, 0);
+		geometry.calculateLocalInertia(massVehicle, localInertia);
+		var body = new Ammo.btRigidBody(new Ammo.btRigidBodyConstructionInfo(massVehicle, motionState, geometry, localInertia));
+		body.setActivationState(DISABLE_DEACTIVATION);
+		physicWorld.addRigidBody(body);
+		var chassisMesh = createChassisMesh(chassisWidth, chassisHeight, chassisLength);
+
+		// 
+		var engineForce = 0;
+		var vehicleSteering = 0;
+		var breakingForce = 0;
+		var tuning = new Ammo.btVehicleTuning();
+		var rayCaster = new Ammo.btDefaultVehicleRaycaster(physicWorld);
+		var vehicle = new Ammo.btRaycastVehicle(tuning, body, rayCaster);
+		vehicle.setCoordinateSystem(0, 1, 2);
+		physicWorld.addAction(vehicle);
+
+		// 车轮
+		var FRONT_LEFT = 0;
+		var FRONT_RIGHT = 1;
+		var BACK_LEFT = 2;
+		var BACK_RIGHT = 3;
+		var wheelMeshes = [];
+		var wheelDirectionCS0 = new Ammo.btVector3(0, -1, 0);
+		var wheelAxleCS = new Ammo.btVector3(-1, 0, 0);
+
+		function addWheel(isFront, pos, radius, width, index) {
+			var wheelInfo = vehicle.addWheel(
+				pos,
+				wheelDirectionCS0,
+				wheelAxleCS,
+				suspensionRestLength,
+				radius,
+				tuning,
+				isFront
+			);
+			wheelInfo.set_m_suspensionStiffness(suspensionStiffness);
+			wheelInfo.set_m_wheelsDampingRelaxation(suspensionDamping);
+			wheelInfo.set_m_wheelsDampingCompression(suspensionCompression);
+			wheelInfo.set_m_frictionSlip(friction);
+			wheelInfo.set_m_rollInfluence(rollInfluence);
+
+			wheelMeshes[index] = createWheelMesh(radius, width);
+		}
+
+		addWheel(true, new Ammo.btVector3(wheelHalfTrackFront, wheelAxisHeightFront, wheelAxisFrontPosition), wheelRadiusFront, wheelWidthFront, FRONT_LEFT);
+		addWheel(true, new Ammo.btVector3(-wheelHalfTrackFront, wheelAxisHeightFront, wheelAxisFrontPosition), wheelRadiusFront, wheelWidthFront, FRONT_RIGHT);
+		addWheel(false, new Ammo.btVector3(-wheelHalfTrackBack, wheelAxisHeightBack, wheelAxisPositionBack), wheelRadiusBack, wheelWidthBack, BACK_LEFT);
+		addWheel(false, new Ammo.btVector3(wheelHalfTrackBack, wheelAxisHeightBack, wheelAxisPositionBack), wheelRadiusBack, wheelWidthBack, BACK_RIGHT);
+
+		// 同步键盘事件+图像+物理
+		function sync(dt) {
+			var speed = vehicle.getCurrentSpeedKmHour();
+			console.log((speed < 0 ? '(R) ' : '') + Math.abs(speed).toFixed(1) + 'km/h');
+
+			breakingForce = 0;
+			engineForce = 0;
+
+			if (actions.acceleration) {			// W 或 ↑：前进
+				if (speed < -1)
+					breakingForce = maxBreakingForce;
+				else
+					engineForce = maxEngineForce;
+			}
+			if (actions.braking) {					// S 或 ↓：后退
+				if (speed > 1)
+					breakingForce = maxBreakingForce;
+				else
+					engineForce = -maxEngineForce;
+			}
+			if (actions.left) {							// A 或 ←：左转
+				if (vehicleSteering < steeringClamp)
+					vehicleSteering += steeringClamp;
+			}
+			else {
+				if (actions.right) {					// D 或 →：右转
+					if (vehicleSteering > -steeringClamp) 
+						vehicleSteering -= steeringClamp;
+				}
+				else {
+					if (vehicleSteering < -steeringClamp)
+						vehicleSteering += steeringClamp;
+					else {
+						if (vehicleSteering > steeringClamp)
+							vehicleSteering -= steeringClamp;
+						else 
+							vehicleSteering = 0;
+					}
+				}
+			}
+		
+			vehicle.applyEngineForce(engineForce, BACK_LEFT);
+			vehicle.applyEngineForce(engineForce, BACK_RIGHT);
+
+			vehicle.setBrake(breakingForce / 2, FRONT_LEFT);
+			vehicle.setBrake(breakingForce / 2, FRONT_RIGHT);
+			vehicle.setBrake(breakingForce, BACK_LEFT);
+			vehicle.setBrake(breakingForce, BACK_RIGHT);
+
+			vehicle.setSteeringValue(vehicleSteering, FRONT_LEFT);
+			vehicle.setSteeringValue(vehicleSteering, FRONT_RIGHT);
+
+			var tm, p, q, i;
+			var n = vehicle.getNumWheels();
+			for (i = 0; i < n; i++) {
+				vehicle.updateWheelTransform(i, true);
+				tm = vehicle.getWheelTransformWS(i);
+				p = tm.getOrigin();
+				q = tm.getRotation();
+				wheelMeshes[i].position.set(p.x(), p.y(), p.z());
+				wheelMeshes[i].quaternion.set(q.x(), q.y(), q.z(), q.w());
+
+				wheelMeshes[i].modelMatrixNeedsUpdate = true;
+			}
+
+			tm = vehicle.getChassisWorldTransform();
+			p = tm.getOrigin();
+			q = tm.getRotation();
+			chassisMesh.position.set(p.x(), p.y(), p.z());
+			chassisMesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+			chassisMesh.modelMatrixNeedsUpdate = true;
+		}
+		syncList.push(sync);
+	}
+
+	// 创建场景中的实体（方块 + 小车）
+	function createObjects() {
+		createBox(new Cesium.Cartesian3(0, -0.5, 0), ZERO_QUATERNION, 75, 1, 75, 0, 2);
+		var quaternion = new Cesium.Quaternion(0, 0, 0, 1);
+		Cesium.Quaternion.fromAxisAngle(new Cesium.Cartesian3(1, 0, 0), -Math.PI / 18, quaternion);
+		createBox(new Cesium.Cartesian3(0, -1.5, 0), quaternion, 8, 4, 10, 0);
+
+		var size = 0.75;
+		var nw = 8;
+		var nh = 6;
+		for (let j = 0; j < nw; j++) {
+			for (let i = 0; i < nh; i++) {
+				createBox(new Cesium.Cartesian3(size * j - (size * (nw - 1)) / 2, size * i, 10), ZERO_QUATERNION, size, size, size, 10);
+			}
+		}
+		createVehicle(new Cesium.Cartesian3(0, 4, -20), ZERO_QUATERNION);
+	}
+
+	var start = false;
+	var init = false; 
+	var startTime = new Date();
+	function update(frameState) {
+		var deltaTime = (new Date() - startTime) / 1000.0;
+		updatePhysics(deltaTime);
+		startTime = new Date();
+	}
+	setTimeout(function() {
+		if (!init) {
+			initGraphics();
+			initPhysics();
+			createObjects();
+			init = true;
+		}
+		if (!start) {
+			meshVisualizer.beforeUpdate.addEventListener(update);
+			start = true;
+		}
+		else {
+			meshVisualizer.beforeUpdate.removeEventListener(update);
+			start = false;
+		}
+	}, 1000 * 3);
+});
 
 // // 创建立方体
 // function createBoxMesh(dmX, dmY, dmZ, scale) {
@@ -237,7 +579,7 @@ Ammo().then(function() {
 
 // 	lastTime = time;
 // });
-
+///////////////////////////////////////////////////////////////////////////////////////////
 // // 示例一：Cesium.Geometry + Cesium.MeshMaterial 组合
 // var box = Cesium.BoxGeometry.createGeometry(Cesium.BoxGeometry.fromDimensions({
 // 	dimensions: new Cesium.Cartesian3(100000, 50000, 50000),
