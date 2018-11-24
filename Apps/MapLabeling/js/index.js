03e47c259321349e1870862a34affec673a3ec61
Thistledown <120768091@qq.com>
2018-11-24 14:22:52 +0800

    添加New Pin按钮

--- Apps/MapLabeling/js/index.js
+++ Apps/MapLabeling/js/index.js
@@ -35,27 +35,9 @@ var scene = viewer.scene;
 
 var pinBuilder = new Cesium.PinBuilder();
 
-var bluePin = viewer.entities.add({
-  name: 'Blank blue pin',
-  position: home,
-  billborad: {
-    image: pinBuilder.fromText('?', Cesium.Color.BLACK, 48).toDataURL(),
-    verticalOrigin: Cesium.VerticalOrigin.BOTTOM
-  }
-});
-
-var questionPin = viewer.entities.add({
-  name : 'Question mark',
-  position : Cesium.Cartesian3.fromDegrees(-75.1698529, 39.9220071),
-  billboard : {
-      image : pinBuilder.fromText('?', Cesium.Color.BLACK, 48).toDataURL(),
-      verticalOrigin : Cesium.VerticalOrigin.BOTTOM
-  }
-});
-
 const handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-var cursorPointLongitude;
-var cursorPointLatitude;
+var cursorPointLongitude;   // 鼠标指针当前经度
+var cursorPointLatitude;    // 鼠标指针当前纬度
 handler.setInputAction(function(movement) {
   var cartesian = scene.camera.pickEllipsoid(movement.endPosition, scene.globe.ellipsoid);
   if (cartesian) {
@@ -63,14 +45,77 @@ handler.setInputAction(function(movement) {
     cursorPointLongitude = Cesium.Math.toDegrees(cartographic.longitude);
     cursorPointLatitude = Cesium.Math.toDegrees(cartographic.latitude);
   }
+  else {
+    cursorPointLongitude = undefined;
+    cursorPointLatitude = undefined;
+  }
 }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
 
-function createPin() {
-  var newPin = viewer.entities.add({
-    name: 'newPin',
-    position: Cesium.Cartesian3.fromDegrees(cursorPointLongitude, cursorPointLatitude),
-    billboard: {
-      image: pinBuilder
-    }
-  })
+
+function createNewPin() {
+  var pinHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+  pinHandler.setInputAction(function(movement) {
+    var newPin = viewer.entities.add({
+      name: 'newPin',
+      position: Cesium.Cartesian3.fromDegrees(cursorPointLongitude, cursorPointLatitude),
+      billboard: {
+        image: pinBuilder.fromText('?', Cesium.Color.BLACK, 48).toDataURL(),
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+      }
+    });
+  }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+  pinHandler.setInputAction(function(movement) {
+    pinHandler.destroy();
+  }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
 }
+
+function showCoordinate() {
+  // HTML overlay for showing feature name on mouseover
+  var coordinateOverlay = document.createElement('div');
+  viewer.container.appendChild(coordinateOverlay);
+  coordinateOverlay.className = 'backdrop';
+  coordinateOverlay.style.display = 'none';
+  coordinateOverlay.style.position = 'absolute';
+  coordinateOverlay.style.bottom = '0';
+  coordinateOverlay.style.left = '0';
+  coordinateOverlay.style['pointerEvents'] = 'none';
+  coordinateOverlay.style.padding = '4px';
+  coordinateOverlay.style.backgroundColor = 'black';
+
+  var coordinateHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+  coordinateHandler.setInputAction(function(movement) {
+    if (cursorPointLatitude != undefined) {   // 存在经纬度
+      // var coordinateString = '经度：' + cursorPointLongitude + '纬度' + cursorPointLatitude;
+      var coordinateString = '\
+      <h1>TEST</h1>\
+      <p>TEST</p>';
+      var coordinateLabel = viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(cursorPointLongitude, cursorPointLatitude),
+        label: {
+          text: coordinateString
+        }
+      });
+      coordinateLabel.label.scale = 0.5;
+      coordinateLabel.label.showBackground = true;
+    }
+    else {                                    // 鼠标在地图外
+      coordinateOverlay.style.display = 'none';
+    }
+  }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+  // 右键单击结束
+  coordinateHandler.setInputAction(function(movement) {
+    coordinateOverlay.style.display = 'none';
+    coordinateHandler.destroy();
+  }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+  
+}
+
+Sandcastle.addToolbarButton('New Pin', function() {
+  return createNewPin();
+})
+Sandcastle.addToolbarButton('Get Coordinate', function() {
+  return showCoordinate();
+})
+Sandcastle.addToolbarButton('Clear All', function() {
+  viewer.entities.removeAll();
+})
