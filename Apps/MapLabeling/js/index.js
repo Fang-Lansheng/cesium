a1d76c4c10ff93fed76b8311d8fd78d81411ffea
Thistledown <120768091@qq.com>
2018-12-27 16:03:21 +0800

    完成 Map Labeling ，实现简单的地图标注功能

--- Apps/MapLabeling/js/index.js
+++ Apps/MapLabeling/js/index.js
@@ -5,32 +5,6 @@ homePosition[2] = 40000;
 initCesium();
 Sandcastle.finishedLoading();
 
-// var Rivers = viewer.dataSources.add(Cesium.KmlDataSource.load('./source/wuhan_river_system.kml', {
-//   camera: viewer.scene.camera, 
-//   canvas: viewer.scene.canvas,
-//   clampToGround: true     // 开启贴地
-// }));
-// Rivers.then(function(dataSource) {
-//   let riverEntities = dataSource.entities.values; // 获取所有对象，一个 Entity 的 ArrayList
-//   for (let i = 0; i < riverEntities.length; i++) {
-//     let entity = riverEntities[i];
-//     if (entity.polygon || entity.polyline) {
-//       viewer.entities.add(entity);
-//       entity.polygon.fill = undefined;
-//       entity.polygon.material = Cesium.Color.BLUE.withAlpha(0.25);
-//       entity.lable = new Cesium.LabelGraphics({
-//         text: entity._name,   // 文字
-//         font: '14px',
-//         color: Cesium.Color.YELLOW,
-//         style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-//         outlineWidth: 1,
-//         pixelOffset: new Cesium.Cartesian2(0, 20)
-//       })
-//     }
-//   }
-//   dataSource.entities.removeAll();
-// });
-
 var scene = viewer.scene;
 var camera = viewer.camera;
 
@@ -62,133 +36,133 @@ handler.setInputAction(function(movement) {
 
 
 
-// 添加新的大头针
-function createNewPin(text, color) {
-  var pinBuilder = new Cesium.PinBuilder();
-  var pinHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-  pinHandler.setInputAction(function(movement) {
-    var newPin = viewer.entities.add({
-      name: 'newPin',
-      position: Cesium.Cartesian3.fromDegrees(cursorPointLongitude, cursorPointLatitude),
-      billboard: {
-        image: pinBuilder.fromText(text, SetColor(color), 48).toDataURL(),
-        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
-      }
-    });
-  }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
-  pinHandler.setInputAction(function(movement) {
-    pinHandler.destroy();
-  }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
-}
-$(function() {
-  var $pin_button = $('#button-new-pin').find('.toolbar-button');
-  var $pin_modal = $('#button-new-pin').find('.modal');
+// // 添加新的大头针
+// function createNewPin(text, color) {
+//   var pinBuilder = new Cesium.PinBuilder();
+//   var pinHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+//   pinHandler.setInputAction(function(movement) {
+//     var newPin = viewer.entities.add({
+//       name: 'newPin',
+//       position: Cesium.Cartesian3.fromDegrees(cursorPointLongitude, cursorPointLatitude),
+//       billboard: {
+//         image: pinBuilder.fromText(text, SetColor(color), 48).toDataURL(),
+//         verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+//       }
+//     });
+//   }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+//   pinHandler.setInputAction(function(movement) {
+//     pinHandler.destroy();
+//   }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+// }
+// $(function() {
+//   var $pin_button = $('#button-new-pin').find('.toolbar-button');
+//   var $pin_modal = $('#button-new-pin').find('.modal');
 
-  // 点击 New Pin 图标时
-  $pin_button.click(function(event) {
-    if ($(event.target).is($pin_button) && $pin_modal.is(':hidden')) {
-      $('.modal').hide(300);
-      $pin_modal.show(300);
-    }
-    else if ($(event.target).is($pin_button) && !$pin_modal.is(':hidden')) {
-      $pin_modal.hide(300);
-    }
-  })
+//   // 点击 New Pin 图标时
+//   $pin_button.click(function(event) {
+//     if ($(event.target).is($pin_button) && $pin_modal.is(':hidden')) {
+//       $('.modal').hide(300);
+//       $pin_modal.show(300);
+//     }
+//     else if ($(event.target).is($pin_button) && !$pin_modal.is(':hidden')) {
+//       $pin_modal.hide(300);
+//     }
+//   })
 
-  // 点击 √ or × 按钮时
-  $pin_modal.click(function(event) {
-    if ($(event.target).is($('.button-cancel'))) {
-      $pin_modal.hide(300);
-    }
-    if ($(event.target).is($('.button-commit'))) {
-      $pin_modal.hide(300);
-    }
-  })
-})
+//   // 点击 √ or × 按钮时
+//   $pin_modal.click(function(event) {
+//     if ($(event.target).is($('.button-cancel'))) {
+//       $pin_modal.hide(300);
+//     }
+//     if ($(event.target).is($('.button-commit'))) {
+//       $pin_modal.hide(300);
+//     }
+//   })
+// })
 
-/**
- * 气泡窗口（左键单击确定位置，右键结束）
- * 实现参考：
- * Cesium 中加入可更随地球移动的气泡 消息框 弹出框 - 山路十八弯，走过多少遍！ - CSDN博客 https://blog.csdn.net/u012539364/article/details/80292605
- * 基于Cesium的通视分析的实现 - cr196的博客 - CSDN博客 https://blog.csdn.net/cr196/article/details/77072814 
- */
-function showPopup() {
-  var popupPick, popupCartesian, popupCartographic, popupLongitude, popupLatitude;
-  var infoDiv;
-  var popupHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-  popupHandler.setInputAction(function(movement) {
-    if (cursorPointLatitude != undefined) {   // 存在经纬度（鼠标指针在地球上）
-      // 世界坐标（x, y, z）
-      popupCartesian = scene.camera.pickEllipsoid(movement.position, scene.globe.ellipsoid);
-      // 屏幕坐标
-      popupPick = Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, popupCartesian);
-      // 地理坐标（弧度）
-      popupCartographic = Cesium.Cartographic.fromCartesian(popupCartesian);
-      popupLongitude = Cesium.Math.toDegrees(popupCartographic.longitude);
-      popupLatitude = Cesium.Math.toDegrees(popupCartographic.latitude);
+// /**
+//  * 气泡窗口（左键单击确定位置，右键结束）
+//  * 实现参考：
+//  * Cesium 中加入可更随地球移动的气泡 消息框 弹出框 - 山路十八弯，走过多少遍！ - CSDN博客 https://blog.csdn.net/u012539364/article/details/80292605
+//  * 基于Cesium的通视分析的实现 - cr196的博客 - CSDN博客 https://blog.csdn.net/cr196/article/details/77072814 
+//  */
+// function showPopup() {
+//   var popupPick, popupCartesian, popupCartographic, popupLongitude, popupLatitude;
+//   var infoDiv;
+//   var popupHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+//   popupHandler.setInputAction(function(movement) {
+//     if (cursorPointLatitude != undefined) {   // 存在经纬度（鼠标指针在地球上）
+//       // 世界坐标（x, y, z）
+//       popupCartesian = scene.camera.pickEllipsoid(movement.position, scene.globe.ellipsoid);
+//       // 屏幕坐标
+//       popupPick = Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, popupCartesian);
+//       // 地理坐标（弧度）
+//       popupCartographic = Cesium.Cartographic.fromCartesian(popupCartesian);
+//       popupLongitude = Cesium.Math.toDegrees(popupCartographic.longitude);
+//       popupLatitude = Cesium.Math.toDegrees(popupCartographic.latitude);
 
-      if (infoDiv) {
-        console.warn('气泡尚未关闭');
-        window.document.getElementById('popupLongitude').innerHTML = popupLongitude.toFixed(4);
-        window.document.getElementById('popupLatitude').innerHTML = popupLatitude.toFixed(4);
-        return false;
-      }
-      else {
-        infoDiv = window.document.createElement('div');
-        infoDiv.id = "trackPopUp";
-        infoDiv.style.display = 'none';
-        infoDiv.innerHTML = 
-          '<div id="trackPopUpContent" class="leaflet-popup" style="top:0;left:0;">' +
-            '<a class="leaflet-popup-close-button" href="javascript:closePopup()">×</a>' +
-            '<div class="leaflet-popup-content-wrapper">' +
-              '<div id="trackPopUpLink" class="leaflet-popup-content" style="max-width:300px; max-height:500px;">' +
-                '<h2>经度：<span id="popupLongitude"></span>° 纬度：<span id="popupLatitude"></span>°</h2>' +
-              '</div>' +
-            '</div>' +
-            '<div class="leaflet-popup-tip-container">' +
-              '<div class="leaflet-popup-tip"></div>' +
-            '</div>' +
-          '</div>';
+//       if (infoDiv) {
+//         console.warn('气泡尚未关闭');
+//         window.document.getElementById('popupLongitude').innerHTML = popupLongitude.toFixed(4);
+//         window.document.getElementById('popupLatitude').innerHTML = popupLatitude.toFixed(4);
+//         return false;
+//       }
+//       else {
+//         infoDiv = window.document.createElement('div');
+//         infoDiv.id = "trackPopUp";
+//         infoDiv.style.display = 'none';
+//         infoDiv.innerHTML = 
+//           '<div id="trackPopUpContent" class="leaflet-popup" style="top:0;left:0;">' +
+//             '<a class="leaflet-popup-close-button" href="javascript:closePopup()">×</a>' +
+//             '<div class="leaflet-popup-content-wrapper">' +
+//               '<div id="trackPopUpLink" class="leaflet-popup-content" style="max-width:300px; max-height:500px;">' +
+//                 '<h2>经度：<span id="popupLongitude"></span>° 纬度：<span id="popupLatitude"></span>°</h2>' +
+//               '</div>' +
+//             '</div>' +
+//             '<div class="leaflet-popup-tip-container">' +
+//               '<div class="leaflet-popup-tip"></div>' +
+//             '</div>' +
+//           '</div>';
         
-        window.document.getElementById('cesiumContainer').appendChild(infoDiv);
-        window.document.getElementById('popupLongitude').innerHTML = popupLongitude.toFixed(4);
-        window.document.getElementById('popupLatitude').innerHTML = popupLatitude.toFixed(4);
-        window.document.getElementById('trackPopUp').style.display = 'block';
-      }
-    }
-  }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+//         window.document.getElementById('cesiumContainer').appendChild(infoDiv);
+//         window.document.getElementById('popupLongitude').innerHTML = popupLongitude.toFixed(4);
+//         window.document.getElementById('popupLatitude').innerHTML = popupLatitude.toFixed(4);
+//         window.document.getElementById('trackPopUp').style.display = 'block';
+//       }
+//     }
+//   }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
   
-  // 使弹窗保持相对位置不变
-  var popupPick_new;    // 新的屏幕坐标
-  viewer.scene.postRender.addEventListener(function() {
-    if (popupPick !== undefined) {
-      if (popupPick_new !== popupPick) {
-        popupPick_new = Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, popupCartesian);
-        var popupWidth = document.getElementById('trackPopUpContent').offsetWidth;
-        var popupHeight = document.getElementById('trackPopUpContent').offsetHeight;
+//   // 使弹窗保持相对位置不变
+//   var popupPick_new;    // 新的屏幕坐标
+//   viewer.scene.postRender.addEventListener(function() {
+//     if (popupPick !== undefined) {
+//       if (popupPick_new !== popupPick) {
+//         popupPick_new = Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, popupCartesian);
+//         var popupWidth = document.getElementById('trackPopUpContent').offsetWidth;
+//         var popupHeight = document.getElementById('trackPopUpContent').offsetHeight;
   
-        var trackPopUpContent = window.document.getElementById('trackPopUpContent');
-        trackPopUpContent.style.left = popupPick_new.x - (popupWidth / 2) + 'px';
-        trackPopUpContent.style.top = popupPick_new.y - (popupHeight - 3) + 'px';
-      }
-    }
-  })
+//         var trackPopUpContent = window.document.getElementById('trackPopUpContent');
+//         trackPopUpContent.style.left = popupPick_new.x - (popupWidth / 2) + 'px';
+//         trackPopUpContent.style.top = popupPick_new.y - (popupHeight - 3) + 'px';
+//       }
+//     }
+//   })
 
-  // 右键单击结束
-  popupHandler.setInputAction(function(movement) {
-    popupHandler.destroy();
-  }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
-}
+//   // 右键单击结束
+//   popupHandler.setInputAction(function(movement) {
+//     popupHandler.destroy();
+//   }, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+// }
 
-/**
- * 关闭气泡窗口
- */
-function closePopup() {
-  var trackPopUp = window.document.getElementById('trackPopUp');
-  if (trackPopUp) {
-    trackPopUp.style.display = 'none';
-  }
-}
+// /**
+//  * 关闭气泡窗口
+//  */
+// function closePopup() {
+//   var trackPopUp = window.document.getElementById('trackPopUp');
+//   if (trackPopUp) {
+//     trackPopUp.style.display = 'none';
+//   }
+// }
 
 // 将 color（string）与 Cesium.Color 一一对应
 function SetColor(color) {
@@ -388,7 +362,71 @@ $(function() {
   })
 })
 
-
 Sandcastle.addToggleButton('天地图注记', viewer.imageryLayers.get(1).show = true, function(checked) {
   viewer.imageryLayers.get(1).show = checked;
 }, 'button-tianditu');
+
+/**
+ * 
+ */
+// 封装 Polyline 对象
+var PolylinePrimitive = (function() {
+  function _(positions) {
+    this.options = {
+      polyline: {
+        show: true,
+        positions: [],
+        material: Cesium.Color.CORNFLOWERBLUE,
+        width: 5
+      }
+    };
+    this.positions = positions;
+    this._init();
+  }
+
+  _.prototype._init = function() {
+    var _self = this;
+    var _update = function() {
+      return _self.positions;
+    };
+
+    // 实时更新 polyline.positions
+    this.options.polyline.positions = new Cesium.CallbackProperty(_update, false);
+    viewer.entities.add(this.options);
+  }
+
+  return _;
+})();
+
+// 初始化
+var positions = [];
+var poly = undefined;
+
+// 鼠标监听事件
+handler.setInputAction(function(movement) {
+  var cartesian = scene.camera.pickEllipsoid(movement.position, scene.globe.ellipsoid);
+  
+  if (positions.length == 0) {
+    positions.push(cartesian.clone());
+  }
+
+  positions.push(cartesian);
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK)
+
+handler.setInputAction(function(movement) {
+  var cartesian = scene.camera.pickEllipsoid(movement.endPosition, scene.globe.ellipsoid);
+
+  if (positions.length >= 2) {
+    if (!Cesium.defined(poly)) {
+      poly = new PolylinePrimitive(positions);
+    } else {
+      positions.pop();
+      cartesian.y += (1 + Math.random());
+      positions.push(cartesian);
+    }
+  }
+}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+handler.setInputAction(function(movement) {
+  handler.destroy();
+}, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
